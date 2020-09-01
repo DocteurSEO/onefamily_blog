7478724ca587cc39c304ae85485360a1747fae23
docteurseo <docteurseoo@gmail.com>
2020-09-01 18:13:59 +0200

    fix db

--- src/db/models/comment.js
+++ src/db/models/comment.js
@@ -1,7 +1,7 @@
 export default {
   CREATE_TABLE: `CREATE TABLE IF NOT EXISTS comments (
     id serial PRIMARY KEY,
-    post_id VARCHAR (50) , 
+    post_id BIGINT , 
     author_id VARCHAR (50) ,
     content text,
     FOREIGN KEY (post_id) REFERENCES posts(id) ON UPDATE CASCADE,
